4c7df7ddcbfeca7fb377706e235360a0e9a07727
Navas V <navas.v@in.abb.com>
2025-01-26 21:23:18 +0530

    Added Flask app, Dockerfile, and related files

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import mlflow
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
